aea3b480c9ddb25d7ff387918e6a1b5aafb790f9
karl <karl.bittner@hexaquo.at>
2020-12-30 20:48:58 +0100

    Allow filtering Video by Name, make Name unique
    The Name field was also removed from the Image as it is not required there.
    
    The Video Data is nullable, as the Frontend should be able to create a template for a new Video to be filled by the Backend.

--- AwesomeTimelapseREST/api/models.py
+++ AwesomeTimelapseREST/api/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 
 class Image(models.Model):
-    name = models.CharField(max_length=60)
     data = models.ImageField(upload_to='images/')
 
     def __str__(self):
-        return self.name
+        return "Image Resource"
 
 
 class Video(models.Model):
-    name = models.CharField(max_length=60)
-    data = models.FileField(upload_to='videos/')
+    name = models.CharField(max_length=60, null=False, unique=True)
+    data = models.FileField(upload_to='videos/', null=True)
 
     def __str__(self):
         return self.name
